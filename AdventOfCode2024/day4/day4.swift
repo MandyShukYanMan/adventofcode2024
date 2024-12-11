110ce09d68cfa6fb6ba4952f24c3388bfbb09d51
Mandy Man <mandyman@MacBookPro.communityfibre.co.uk>
2024-12-11 21:23:50 +0000

    Add day 4 part 2

--- AdventOfCode2024/day4/day4.swift
+++ AdventOfCode2024/day4/day4.swift
@@ -16,7 +16,54 @@ func runDay4() throws {
     let lines = fileContents.split(separator: "\n").map { Array($0) }
     let foundWordsCount = countAllMatchingWords(lines: lines, xIndex: 0, yIndex: 0)
     print("Found words: ", foundWordsCount)
+    let foundCrosses = countAllCrossesInLines(lines, 0, 0)
+    print("Found X MAS crosses: ", foundCrosses)
 }
+
+func countAllCrossesInLines(_ lines: [[Substring.Element]], _ xIndex: Int, _ yIndex: Int) -> Int {
+    if yIndex >= lines.count {
+        return 0
+    }
+    let currentLine = lines[yIndex]
+    if xIndex >= currentLine.count {
+        return countAllCrossesInLines(lines, 0, yIndex + 1)
+    }
+    var foundCrosses = 0
+    if (letterSearch(lines, "A", xIndex, yIndex)) {
+        let leftMas = letterSearch(lines, "M", xIndex-1, yIndex-1) && letterSearch(lines, "S", xIndex+1, yIndex+1)
+        let leftSam = letterSearch(lines, "S", xIndex-1, yIndex-1) && letterSearch(lines, "M", xIndex+1, yIndex+1)
+        let rightMas = letterSearch(lines, "M", xIndex+1, yIndex-1) && letterSearch(lines, "S", xIndex-1, yIndex+1)
+        let rightSam = letterSearch(lines, "S", xIndex+1, yIndex-1) && letterSearch(lines, "M", xIndex-1, yIndex+1)
+        if (leftMas || leftSam) && (rightMas || rightSam) {
+            foundCrosses += 1
+        }
+    }
+ 
+ 
+    return foundCrosses + countAllCrossesInLines(lines, xIndex + 1, yIndex)
+}
+
+func letterSearch(
+    _ lines: [[Substring.Element]],
+    _ letter: Substring.Element,
+    _ xIndex: Int,
+    _ yIndex: Int
+) -> Bool {
+    if yIndex < 0 || yIndex >= lines.count {
+        return false
+    }
+    let currentLine = lines[yIndex]
+    if xIndex < 0 || xIndex >= currentLine.count {
+        return false
+    }
+    if currentLine[xIndex] == letter {
+        return true
+    }
+    return false
+}
+
+
+
 
 func countAllMatchingWords(lines: [[Substring.Element]], xIndex: Int, yIndex: Int) -> Int {
     if yIndex >= lines.count {
